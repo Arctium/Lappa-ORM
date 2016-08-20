f17a012df0d8e1c88d412710c8a5672b4375f178
Fabian <fabian@arctium.io>
2016-08-20 19:41:02 +0200

    Added global 'Set' extension method to allow setting values in lambdas.

---
+++ Lappa ORM/Misc/GlobalExtensions.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (C) Arctium Software.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+public static class GlobalExtensions
+{
+    // Used for set expression in our Database.Update method.
+    public static T Set<T>(this T @var, T value) => var = value;
+}

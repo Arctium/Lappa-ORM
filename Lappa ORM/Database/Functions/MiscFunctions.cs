ed01ff08bef43143549defcbbd9a08f2fc219eff
Fabian <fabian@arctium.io>
2016-12-13 17:22:38 +0100

    Fixed wrong using...

--- Lappa ORM/Database/Functions/MiscFunctions.cs
+++ Lappa ORM/Database/Functions/MiscFunctions.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Framework.Logging;
+using LappaORM.Logging;
 using LappaORM.Misc;
 using static LappaORM.Misc.Helper;
 

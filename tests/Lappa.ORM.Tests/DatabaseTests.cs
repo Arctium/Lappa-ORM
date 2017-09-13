905406c3d417eadba93a1816962b43681a3d147c
nevotheless <kopplow.tim@gmail.com>
2017-09-13 10:15:16 +0200

    Added first xunit test (#15)
    Added more testing relevant dependencies and a first test

---
+++ tests/Lappa.ORM.Tests/DatabaseTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using Lappa.ORM.Constants;
+
+namespace Lappa.ORM.Tests
+{
+    public class DatabaseTests
+    {
+        [Fact]
+        public void SQLiteDatabaseConnection()
+        {
+            var db = new Database();
+            var initSuccess = db.Initialize("Data Source=:memory:", DatabaseType.SQLite);
+
+            Assert.True(initSuccess);
+        }
+    }
+}

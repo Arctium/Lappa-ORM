7e2656f859cfcc21c0632da5ea82852ad02a89f7
Fabian <fabian@arctium.io>
2019-09-02 14:34:28 +0200

    Updated nuget packages & submodules. Remove AnyCPU build configuration. Only x64 is supported now. Supress CA2100 security warnings. We don't allow any direct user input there.

---
+++ src/Lappa.ORM/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "<Pending>", Scope = "member", Target = "~M:Lappa.ORM.Database.CreateSqlCommand(System.Data.Common.DbConnection,System.Data.Common.DbTransaction,Lappa.ORM.IQueryBuilder)~System.Data.Common.DbCommand")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "<Pending>", Scope = "member", Target = "~M:Lappa.ORM.Database.CreateSqlCommand(System.Data.Common.DbConnection,System.Data.Common.DbTransaction,Lappa.ORM.ApiRequest)~System.Data.Common.DbCommand")]

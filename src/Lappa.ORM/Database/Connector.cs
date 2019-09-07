16e0e1f388adb5924e6d2fae25cc26f862f3e6fc
Fabian <fabian@arctium.io>
2019-09-07 20:19:47 +0200

    Include all supported connectors directly into the ORM as nuget package.

--- src/Lappa.ORM/Database/Connector.cs
+++ src/Lappa.ORM/Database/Connector.cs
@@ -3,10 +3,14 @@
 
 using System;
 using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
 using Lappa.ORM.Constants;
 using Lappa.ORM.Misc;
+using Microsoft.Data.Sqlite;
+using MySql.Data.MySqlClient;
+using Npgsql;
 
 namespace Lappa.ORM
 {
@@ -15,7 +19,6 @@ namespace Lappa.ORM
         internal ConnectorSettings Settings { get; set; }
         internal ConnectorQuery Query { get; private set; }
 
-        Assembly assembly;
         Type connectionType;
         Type commandType;
         Type parameterType;
@@ -24,41 +27,30 @@ namespace Lappa.ORM
         {
             Query = new ConnectorQuery(Settings.DatabaseType);
 
-            // Use MSSql as default.
-            var typeBase = "System.Data.SqlClient.Sql";
-
             switch (Settings.DatabaseType)
             {
                 case DatabaseType.MSSql:
                 {
-                    assembly = Assembly.Load(new AssemblyName("System.Data.SqlClient, Version=4.5.1.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"));
+
+                    connectionType = typeof(SqlConnection);
+                    commandType = typeof(SqlCommand);
+                    parameterType = typeof(SqlParameter);
+
                     break;
                 }
                 case DatabaseType.MySql:
                 {
-                    typeBase = "MySql.Data.MySqlClient.MySql";
-
-                    if (!string.IsNullOrEmpty(Settings.ConnectorPath))
-                        assembly = Assembly.LoadFrom(Settings.ConnectorPath);
-                    else
-                    {
-                        var mysqlAssemblyName = AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(asm => asm.GetValidTypes().
-                                                                                                               Any(t => t.Name.StartsWith("MySql.Data") ||
-                                                                                                                        t.Name.StartsWith("MySqlConnector")));
-
-                        // Let's throw a type load exception if no supported MySql lib is found.
-                        if (mysqlAssemblyName == null)
-                            throw new TypeLoadException("No assembly referencing 'MySql' found.");
-
-                        assembly = Assembly.Load(mysqlAssemblyName.FullName);
-                    }
+                    connectionType = typeof(MySqlConnection);
+                    commandType = typeof(MySqlCommand);
+                    parameterType = typeof(MySqlParameter);
 
                     break;
                 }
                 case DatabaseType.SQLite:
                 {
-                    typeBase = "Microsoft.Data.Sqlite.Sqlite";
-                    assembly = Assembly.Load(new AssemblyName("Microsoft.Data.Sqlite, Version=2.1.0.0, Culture=neutral, PublicKeyToken=adb9793829ddae60"));
+                    connectionType = typeof(SqliteConnection);
+                    commandType = typeof(SqliteCommand);
+                    parameterType = typeof(SqliteParameter);
 
                     break;
                 }
@@ -66,30 +58,15 @@ namespace Lappa.ORM
                     throw new NotSupportedException("Oracle is not supported.");
                 case DatabaseType.PostgreSql:
                 {
-                    typeBase = "Npgsql";
-
-                    if (!string.IsNullOrEmpty(Settings.ConnectorPath))
-                        assembly = Assembly.LoadFrom(Settings.ConnectorPath);
-                    else
-                    {
-                        var npgsqlAssemblyName = AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(asm => asm.GetValidTypes().Any(t => t.Name.StartsWith("Npgsql")));
-
-                        // Let's throw a type load exception if no supported MySql lib is found.
-                        if (npgsqlAssemblyName == null)
-                            throw new TypeLoadException("No assembly referencing 'Npgsql' found.");
-
-                        assembly = Assembly.Load(npgsqlAssemblyName.FullName);
-                    }
+                    connectionType = typeof(NpgsqlConnection);
+                    commandType = typeof(NpgsqlCommand);
+                    parameterType = typeof(NpgsqlParameter);
 
                     break;
                 }
                 default:
                     break;
             }
-
-            connectionType = assembly.GetType($"{typeBase}Connection");
-            commandType = assembly.GetType($"{typeBase}Command");
-            parameterType = assembly.GetType($"{typeBase}Parameter");
 
             if (connectionType == null || commandType == null || parameterType == null)
                 throw new TypeLoadException($"connectionType: {connectionType}, commandType: {commandType}, parameterType: {parameterType}.");
